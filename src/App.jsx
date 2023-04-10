f97c0c026af6656f29b2f4c1865d83a6d6a5f1ee
gdumani <gdumani@gmail.com>
2023-04-10 15:13:44 -0600

    add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Navbar from './components/Navbar'
+import './Styles/App.css';
+import Navbar from './components/Navbar';
 
 
-function App() {
+const  App = () => {
   const [count, setCount] = useState(0)
 
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import planetIcon from '../assets/planet.png';
 import { Link } from 'react-router-dom';
+import '../Styles/Navbar.css';
 
 const Navbar = () => {
   return (
     <header>
-      <nav>
-        <img src={planetIcon} alt="Planet Icon" />
-        <h1>Space Travelers&apos; Hub </h1>
-        <ul>
+      <nav className='container'>
+        <div>
+          <img className='logo' src={planetIcon} alt="Planet Icon" />
+          <h1>Space Travelers&apos; Hub </h1>
+        </div>
+        <ul className='linksList'>
           <li>
             <Link to='/'>Rockets</Link>
           </li>
