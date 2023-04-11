301d79d2e843f235053556a095997304f023c595
gdumani <gdumani@gmail.com>
2023-04-11 13:58:09 -0600

    create rocket slice with logic for API retrieval

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const RocketUrl = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  const response = await axios.get(RocketUrl).then((res) => res.data.map((el) => (
+    {
+      roketId: el.id,
+      rocketName: el.rocket_name,
+      rocketDescription: el.description,
+      rocketImage: el.flickr_images[0],
+      rocketStatus: false,
+    }
+  )));
+  return response;
+});
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.fulfilled, (state, action) => action.payload);
+  },
+});
+
+export default rocketsSlice.reducer;
