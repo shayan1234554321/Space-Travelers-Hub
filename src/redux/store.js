867daf141b3a79c6f11e1786c2387f57a412e103
gdumani <gdumani@gmail.com>
2023-04-11 14:09:09 -0600

    add rockets reducer to store and updated eslintrc

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: { rocketsReducer },
 });
 
 export default store;
