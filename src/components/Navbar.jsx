b9316034edc42d1055856dfaf18accd583af9d15
gdumani <gdumani@gmail.com>
2023-04-13 17:33:10 -0600

    clean Navbar and check proptypes in missions (still pending)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import planetIcon from '../assets/planet.png';
 import '../Styles/Navbar.css';
 
-const Navbar = () => {
-  const [active, setActive] = useState('rockets');
-
-  return (
-    <header>
-      <nav className="container">
-        <div>
-          <img className="logo" src={planetIcon} alt="Planet Icon" />
-          <h1>Space Travelers&apos; Hub </h1>
-        </div>
-        <ul className="linksList">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive, isPending }) => (isActive ? 'active' : '')}
-            >
-              Rockets
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/missions"
-              className={({ isActive, isPending }) => (isActive ? 'active' : '')}
-            >
-              Missions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/profile"
-              className={({ isActive, isPending }) => (isActive ? 'active' : '')}
-            >
-              My Profile
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
+const Navbar = () => (
+  <header>
+    <nav className="container">
+      <div>
+        <img className="logo" src={planetIcon} alt="Planet Icon" />
+        <h1>Space Travelers&apos; Hub </h1>
+      </div>
+      <ul className="linksList">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            Rockets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/missions"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            Missions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            My Profile
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 export default Navbar;
